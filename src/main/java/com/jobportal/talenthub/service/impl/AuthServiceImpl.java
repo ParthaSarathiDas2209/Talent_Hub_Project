45cbb8462aac8e234d5e77fe3a984bb62ec7241d
Partha Sarathi Das <parthasarathidas0922@gmail.com>
2026-07-15 01:09:21 +0530

    Completed Spring Security and JWT Authentication

--- src/main/java/com/jobportal/talenthub/service/impl/AuthServiceImpl.java
+++ src/main/java/com/jobportal/talenthub/service/impl/AuthServiceImpl.java
@@ -10,17 +10,21 @@ import com.jobportal.talenthub.mapper.UserMapper;
 import com.jobportal.talenthub.repository.UserRepository;
 import com.jobportal.talenthub.service.AuthService;
 import com.jobportal.talenthub.service.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
 
-    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
@@ -44,21 +48,19 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public AuthResponseDto login(LoginRequestDto loginRequestDto) {
 
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequestDto.email(),
+                        loginRequestDto.password()
+                )
+        );
+
         User user = userRepository.findByEmail(loginRequestDto.email())
                 .orElseThrow(() ->
                         new InvalidCredentialsException(
                                 "Invalid e-mail or password")
                 );
-
-        boolean isPasswordValid = passwordEncoder.matches(
-                loginRequestDto.password(),
-                user.getPassword()
-        );
-
-        if (!isPasswordValid) {
-            throw new InvalidCredentialsException("Invalid email or password");
-        }
-
+        
         String token = jwtService.generateToken(user.getEmail());
 
         return new AuthResponseDto(
